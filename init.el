938469d08f55878194e78438ef816779e86c0be6
arehnbymartin <arehnbymartin@abebooks.com>
2017-08-15 13:29:16 -0700

    Improve org-mode agenda configuration

--- init.el
+++ init.el
@@ -61,7 +61,7 @@ values."
    ;; wrapped in a layer. If you need some configuration for these
    ;; packages, then consider creating a layer. You can also put the
    ;; configuration in `dotspacemacs/user-config'.
-   dotspacemacs-additional-packages '(git-auto-commit-mode ensime sbt-mode groovy-mode groovy-imports)
+   dotspacemacs-additional-packages '(git-auto-commit-mode ensime sbt-mode groovy-mode groovy-imports sql-indent org-super-agenda)
    ;; A list of packages and/or extensions that will not be install and loaded.
    dotspacemacs-excluded-packages '()
    ;; If non-nil spacemacs will delete any orphan packages, i.e. packages that
@@ -358,16 +358,18 @@ you should place your code here."
   ( setq-default org-agenda-files '("~/org"))
   ( setq org-default-notes-file "~/org/notes.org" )
 
+  ( setq org-hierarchical-todo-statistics nil)
+
   ( setq org-todo-keywords
          '(
-           (sequence "REPEAT"
-                     "STOPPED")
            (sequence
                      "FUTURE"
+                     "WAITING"
                      "TODO"
-                     "DOING"
                      "|"
                      "DONE"
+                     "DELEGATED"
+                     "CANCELLED"
                      )))
 
   ;; custom todo display bound to SPC o c
@@ -378,6 +380,28 @@ you should place your code here."
     (org-agenda arg "g"))
   (spacemacs/set-leader-keys "ot" 'org-agenda-show-todos)
 
+  (setq org-refile-targets '(
+                              ("~/org/plan.org" :maxlevel . 1)
+                              ("~/org/abe.org" :maxlevel . 4)
+                              ))
+
+  ;; super agenda
+  (org-super-agenda-mode)
+  (setq org-super-agenda-groups
+         '(
+           (:name "Today"
+                  :time-grid t)
+           (:name "Deadlines"
+                  :deadline t)
+           (:name "Work"
+                        :tag ("work" "abe"))
+           (:name "Personal"
+                  :tag "personal"
+                  :and (:habit t
+                  :not (:tag "work")))
+           (:auto-category t)
+           )) 
+
   ;; schedule bound to SPC o a
   (defun org-agenda-show-schedule (&optional arg)
     (interactive "P")
@@ -386,10 +410,10 @@ you should place your code here."
 
   ( setq org-capture-templates
          ;; todos
-         '( ("t" "todo" entry (file "~/org/todo.org")
+         '( ("t" "todo" entry (file "~/org/inbox.org")
                   "* TODO %?\n %i\n %a" :clock-in t :clock-resume t)
          ;; general notes
-         ("n" "note" entry (file "~/org/mynotes.org")
+         ("n" "note" entry (file "~/org/inbox.org")
                   "* NOTE %?\n %i\n %a" :clock-in t :clock-resume t)
 
          ;; local notes that won't be pushed to git
@@ -408,6 +432,14 @@ you should place your code here."
   (spacemacs/set-leader-keys
     "oq" 'org-columns-quit)
 
+  (defun magit-diff-master (&optional args)
+    "Show diff range develop...HEAD"
+    (interactive)
+    (magit-diff "master" args))
+  (spacemacs/set-leader-keys "gdm" 'magit-diff-master)
+  (spacemacs/set-leader-keys "gdd" 'magit-diff)
+
+
   ( spacemacs/set-leader-keys-for-major-mode 'c++-mode "j" 'semantic-ia-fast-jump)
 
   ( setq python-shell-interpreter "c:/Users/Alrehn/Anaconda3/Scripts/ipython3.exe")
@@ -423,6 +455,7 @@ you should place your code here."
   (add-to-list 'org-babel-default-header-args:sql '(:engine . "postgresql"))
   (add-to-list 'org-babel-default-header-args:sql '(:cmdline . "-h studentdb.csc.uvic.ca -U alrm imdb"))
   (add-to-list 'org-babel-default-header-args:sql '(:exports . "both"))
+
 
   ;; make this a variable
   (add-hook 'c++-mode-hook
@@ -442,13 +475,16 @@ you should place your code here."
 ;; Do not write anything past this comment. This is where Emacs will
 ;; auto-generate custom variable definitions.
 (custom-set-variables
+ '(org-modules
+   (quote
+    (org-habit)))
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (helm-gtags ggtags groovy-imports pcache winum fuzzy yaml-mode org-outlook groovy-mode mmm-mode markdown-toc markdown-mode gh-md ensime sbt-mode scala-mode xterm-color shell-pop multi-term eshell-z eshell-prompt-extras esh-help web-beautify livid-mode skewer-mode simple-httpd json-mode json-snatcher json-reformat js2-refactor multiple-cursors js2-mode js-doc company-tern dash-functional tern coffee-mode reveal-in-osx-finder pbcopy osx-trash osx-dictionary launchctl sql-indent org goto-chg undo-tree diminish yapfify uuidgen py-isort org-projectile org-download live-py-mode link-hint hide-comnt git-link flyspell-correct-helm flyspell-correct eyebrowse evil-visual-mark-mode evil-unimpaired evil-ediff dumb-jump company-emacs-eclim eclim column-enforce-mode stickyfunc-enhance srefactor helm-flyspell auto-dictionary pyvenv pytest pyenv-mode py-yapf pip-requirements hy-mode helm-pydoc cython-mode company-anaconda anaconda-mode pythonic f git-auto-commit-mode rcirc-notify rcirc-color ws-butler window-numbering volatile-highlights vi-tilde-fringe toc-org spaceline s powerline smooth-scrolling smeargle restart-emacs rainbow-delimiters popwin persp-mode pcre2el paradox hydra spinner page-break-lines orgit org-repo-todo org-present org-pomodoro alert log4e gntp org-plus-contrib org-bullets open-junk-file neotree move-text magit-gitflow macrostep lorem-ipsum linum-relative leuven-theme info+ indent-guide ido-vertical-mode hungry-delete htmlize hl-todo highlight-parentheses highlight-numbers parent-mode highlight-indentation help-fns+ helm-themes helm-swoop helm-projectile helm-mode-manager helm-make projectile helm-gitignore request helm-flx helm-descbinds helm-company helm-c-yasnippet helm-ag google-translate golden-ratio gnuplot gitignore-mode gitconfig-mode gitattributes-mode git-timemachine git-messenger flycheck-pos-tip flycheck pkg-info epl flx-ido flx fill-column-indicator fancy-battery expand-region exec-path-from-shell evil-visualstar evil-tutor evil-surround evil-search-highlight-persist evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-magit magit magit-popup git-commit with-editor evil-lisp-state smartparens evil-indent-plus evil-iedit-state iedit evil-exchange evil-escape evil-args evil-anzu anzu eval-sexp-fu highlight elisp-slime-nav disaster define-word company-statistics company-quickhelp pos-tip company-c-headers company cmake-mode clean-aindent-mode clang-format buffer-move bracketed-paste auto-yasnippet yasnippet auto-highlight-symbol auto-compile packed dash aggressive-indent adaptive-wrap ace-window ace-link ace-jump-helm-line helm avy helm-core async ac-ispell auto-complete popup quelpa package-build use-package which-key bind-key bind-map evil spacemacs-theme)))
+    (org-super-agenda ht helm-gtags ggtags groovy-imports pcache winum fuzzy yaml-mode org-outlook groovy-mode mmm-mode markdown-toc markdown-mode gh-md ensime sbt-mode scala-mode xterm-color shell-pop multi-term eshell-z eshell-prompt-extras esh-help web-beautify livid-mode skewer-mode simple-httpd json-mode json-snatcher json-reformat js2-refactor multiple-cursors js2-mode js-doc company-tern dash-functional tern coffee-mode reveal-in-osx-finder pbcopy osx-trash osx-dictionary launchctl sql-indent org goto-chg undo-tree diminish yapfify uuidgen py-isort org-projectile org-download live-py-mode link-hint hide-comnt git-link flyspell-correct-helm flyspell-correct eyebrowse evil-visual-mark-mode evil-unimpaired evil-ediff dumb-jump company-emacs-eclim eclim column-enforce-mode stickyfunc-enhance srefactor helm-flyspell auto-dictionary pyvenv pytest pyenv-mode py-yapf pip-requirements hy-mode helm-pydoc cython-mode company-anaconda anaconda-mode pythonic f git-auto-commit-mode rcirc-notify rcirc-color ws-butler window-numbering volatile-highlights vi-tilde-fringe toc-org spaceline s powerline smooth-scrolling smeargle restart-emacs rainbow-delimiters popwin persp-mode pcre2el paradox hydra spinner page-break-lines orgit org-repo-todo org-present org-pomodoro alert log4e gntp org-plus-contrib org-bullets open-junk-file neotree move-text magit-gitflow macrostep lorem-ipsum linum-relative leuven-theme info+ indent-guide ido-vertical-mode hungry-delete htmlize hl-todo highlight-parentheses highlight-numbers parent-mode highlight-indentation help-fns+ helm-themes helm-swoop helm-projectile helm-mode-manager helm-make projectile helm-gitignore request helm-flx helm-descbinds helm-company helm-c-yasnippet helm-ag google-translate golden-ratio gnuplot gitignore-mode gitconfig-mode gitattributes-mode git-timemachine git-messenger flycheck-pos-tip flycheck pkg-info epl flx-ido flx fill-column-indicator fancy-battery expand-region exec-path-from-shell evil-visualstar evil-tutor evil-surround evil-search-highlight-persist evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-magit magit magit-popup git-commit with-editor evil-lisp-state smartparens evil-indent-plus evil-iedit-state iedit evil-exchange evil-escape evil-args evil-anzu anzu eval-sexp-fu highlight elisp-slime-nav disaster define-word company-statistics company-quickhelp pos-tip company-c-headers company cmake-mode clean-aindent-mode clang-format buffer-move bracketed-paste auto-yasnippet yasnippet auto-highlight-symbol auto-compile packed dash aggressive-indent adaptive-wrap ace-window ace-link ace-jump-helm-line helm avy helm-core async ac-ispell auto-complete popup quelpa package-build use-package which-key bind-key bind-map evil spacemacs-theme)))
  '(safe-local-variable-values
    (quote
     ((company-clang-arguments "-Ic:/vs_dev_lib/include/")
